1903fb3700681c7d014c80c63b04d17bbf14fee0
klausdev <nickoklaus5@gmail.com>
2025-09-22 08:12:44 +0100

    added advanced keys

--- cooperate-tenant-app/src/config/index.ts
+++ cooperate-tenant-app/src/config/index.ts
@@ -3,7 +3,9 @@ export const config = {
     frontendUrl: import.meta.env.VITE_FRONTEND_URL || 'https://cooperate-frontend.vercel.app',
     localBackendUrl: import.meta.env.VITE_LOCAL_BACKEND_URL || 'http://localhost:5000/api/v1',
     backendLiveUrl: import.meta.env.VITE_BACKEND_LIVE_URL || 'https://cooperative-tenant-1.onrender.com/api/v1',
-  
+
+    advanced_key: import.meta.env.VITE_ADVANCED_KEY || 'ueiweuwewiewoieuoweuwoieuweoiuwueio',
+    advanced_iv: import.meta.env.VITE_ADVANCED_IV || 'ueiweuwewiewoieuoweuwoieuweoiuwueio',
 
     paystackSecretKey: import.meta.env.VITE_PAYSTACK_SECRET_KEY || 'sk_test_83959a91461ee2991a85c19f62faf5b029a3af85',
     paystackPublicKey: import.meta.env.VITE_PAYSTACK_PUBLIC_KEY || 'pk_test_85d208d2ca0e7d0ab79573bda2cc9542054d3980',
